b51f40a183605e7acc7c0678162b539578e2f24e
epgokce <ep.gokce@gmail.com>
2023-11-24 20:32:36 +0300

    Added Redux store with Provider for state

--- src/index.js
+++ src/index.js
@@ -3,15 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { legacy_createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from "redux-thunk"
+import { reducerGratitudeJournal } from "./reducers"
 
+const middleware = composeWithDevTools(applyMiddleware(thunk));
 
+const reducers = combineReducers({
+
+    gratitudeJournal: reducerGratitudeJournal
+
+});
+const store = legacy_createStore(reducers, middleware);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
+  <Provider store={store}>
   <BrowserRouter>
     <>
       <App />
     </>
   </BrowserRouter>
+  </Provider>
 );
