7cd7f434a6be7b16a87b7ae8ee43ea6dc428937f
epgokce <ep.gokce@gmail.com>
2023-11-27 09:35:15 +0300

    Restructured API calls and error handling

--- src/actions.js
+++ src/actions.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const NOT_EKLE = "NOT_EKLE"
 export const NOT_SIL = "NOT_SIL"
+export const GOT_ORDER_REQUIRING_API = "GOT_ORDER_REQUIRING_API"
+export const GOT_ERROR = "GOT ERROR";
 
 export function notEkle(not) {
   // ...
@@ -10,9 +12,19 @@ export function notEkle(not) {
 
 export function notSil(notId) {
   // ...
+  return { type: NOT_SIL, payload: notId };
+}
+
+function gotError(error) {
+  return { type: GOT_ERROR, payload: error};
+}
+
+function gotOrderRequiringApi () {
+  return {type: GOT_ORDER_REQUIRING_API};
 }
 
 export const notEkleAPI = (yeniNot) => dispatch => {
+  dispatch(gotOrderRequiringApi());
   axios
     .post("https://httpbin.org/anything", yeniNot)
     .then((res) => {
@@ -27,13 +39,16 @@ export const notEkleAPI = (yeniNot) => dispatch => {
 }
 
 export const notSilAPI = (id) => dispatch => {
-  console.log(id)
+  dispatch(gotOrderRequiringApi());
   axios
     .delete("https://httpbin.org/anything", { data: id })
     .then((res) => {
       if (res.status === 200) {
         // res.data objesi içerisinden ihtiyaç duyduğunuz değeri bulun ve oluşturduğunuz notSil ile dispatch edin 
+        dispatch(notSil(res.data.data));
       }
     })
-    .catch((error) => console.log(error));
+    .catch((error) => {
+      dispatch(gotError(error.message));
+  });
 }
